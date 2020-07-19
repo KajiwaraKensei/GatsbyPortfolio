fbfa1ff2abe064eac220fb59bfc78a3fa88110b2
KajiwaraKensei <kd1270632@st.kobedenshi.ac.jp>
2020-07-20 02:22:43 +0900

    loading test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styled from "styled-components"
 import Top from "./TopPage"
 import "./index.css"
-
+import Loading from "~/Parts/Loading"
 type Props = {
   className?: string
 }
 
 const App: React.FC<Props> = props => {
   const { className } = props
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  }, [])
   return (
     <div className={className}>
+
       <Top />
+      {loading && <Loading />}
     </div>
   )
 }
---
+++ src/components/Parts/Loading/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./Style.css"
+type Props = {
+}
+
+const Component: React.FC<Props> = props => {
+  const { } = props
+  return <div className="loading">
+    Loading
+  </div>
+}
+
+export default Component
