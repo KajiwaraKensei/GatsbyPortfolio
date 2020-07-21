67fe900afa7d9bd340478d0a738b77db29f08620
KajiwaraKensei <kd1270632@st.kobedenshi.ac.jp>
2020-07-22 03:33:07 +0900

    seo init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import App from "~/components/App"
-
+import SEO from "~/components/SEO"
 import styled from "styled-components"
 
 interface Props {
@@ -9,6 +9,7 @@ interface Props {
 
 const Component: React.FC<Props> = props => (
   <div className={props.className}>
+    <SEO />
     <App />
   </div>
 )
--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -1,9 +1,11 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import SEO from "~/components/SEO"
 
 type Props = {}
 const Component: React.FC<Props> = props => (
   <div>
+    <SEO />
     <h1>Hello site</h1>
     <pre>{JSON.stringify(props)}</pre>
     <Link to="/">Home</Link>
