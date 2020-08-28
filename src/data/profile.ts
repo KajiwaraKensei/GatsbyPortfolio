1d7c75fe9354a82c5def10cc58536c6f4dba40b7
KajiwaraKensei <kd1270632@st.kobedenshi.ac.jp>
2020-08-29 01:51:28 +0900

    月を追加

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -24,9 +24,9 @@ export const profile: profile = {
   image: "profile.jpg",
   careers: [{ year: "2021 ~ ", event: "とある会社に内定済み" }],
   introductions: [
-    { year: "2018", event: "岡山県立岡山操山高等学校 卒業" },
-    { year: "2018", event: "神戸電子専門学校 ITスペシャリリスト学科 入学" },
-    { year: "2021 ", event: "神戸電子専門学校 卒業見込み" },
+    { year: "2018 03", event: "岡山県立岡山操山高等学校 卒業" },
+    { year: "2018 04", event: "神戸電子専門学校 ITスペシャリリスト学科 入学" },
+    { year: "2021 03 ", event: "神戸電子専門学校 卒業見込み" },
   ],
   selfIntroduction:
     "趣味は参考書を買うことです。\nMy hobby is to buy reference books. I have purchased over 30 books so far.",
